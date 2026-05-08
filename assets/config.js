0c2912ae4c2b148b218b3f07c022f9c352091fb7
daanz <danzpreset456@gmail.com>
2026-05-08 22:39:18 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 const GITHUB_USER = 'danz-my';
-const GITHUB_REPO = 'snippets_dann';
+const GITHUB_REPO = 'snippets-dann';
 const BRANCH = 'main';
 const RAW_BASE = `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/${BRANCH}`;
 const SNIPPETS_FILE = 'snippets.json';
