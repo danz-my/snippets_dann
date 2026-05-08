213f7086d5d15e13fd04ce697caecc5c816668c3
daanz <danzpreset456@gmail.com>
2026-05-08 22:34:17 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,5 @@
-// ============================================
-// CONFIG — Ganti sesuai GitHub repo lo
-// ============================================
 const GITHUB_USER = 'snippets_dann';
 const GITHUB_REPO = 'danz-my';
-const RAW_BASE    = `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main`;
+const BRANCH = 'main';
+const RAW_BASE = `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/${BRANCH}`;
 const SNIPPETS_FILE = 'snippets.json';
