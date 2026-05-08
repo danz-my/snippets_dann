1dafff1baa97c35b0f4560b72adaf437cb2cee81
daanz <danzpreset456@gmail.com>
2026-05-08 22:11:40 +0700

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,7 @@
+// ============================================
+// CONFIG — Ganti sesuai GitHub repo lo
+// ============================================
+const GITHUB_USER = 'snippets_dann';
+const GITHUB_REPO = 'danz-my';
+const RAW_BASE    = `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/main`;
+const SNIPPETS_FILE = 'snippets.json';
