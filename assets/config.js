0e09f95a4c0672faad4488134e92d4ef5a6933ba
daanz <danzpreset456@gmail.com>
2026-06-11 05:58:05 +0700

    Delete assets directory

--- assets/config.js
+++
@@ -1,5 +0,0 @@
-const GITHUB_USER = 'danz-my';
-const GITHUB_REPO = 'snippets-dann';
-const BRANCH = 'main';
-const RAW_BASE = `https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/${BRANCH}`;
-const SNIPPETS_FILE = 'snippets.json';
